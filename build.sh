9e3a08ed2a7d24e40ad9a04fa972befffdaca0d2
Suhas R <rsuhas319@gmail.com>
2025-02-26 22:57:00 +0530

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies
+apt-get update && apt-get install -y cmake build-essential pkg-config libx11-dev libatlas-base-dev libgtk-3-dev libboost-python-dev
+
+# Upgrade pip
 python -m pip install --upgrade pip
+
+# Install Python packages
 pip install -r requirements.txt 
